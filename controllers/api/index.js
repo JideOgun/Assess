ef164e43afd210c48666595594b9cab3ab41039e
JideOgun <JideOgun@github.com>
2022-02-17 20:13:50 -0600

    company routes working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 
 const reviewsRoutes = require('./reviews-routes');
-
+const companyRoutes = require('./company-routes');
 
 router.use('/reviews', reviewsRoutes);
+router.use('/companies', companyRoutes);
+
 
 module.exports = router;
