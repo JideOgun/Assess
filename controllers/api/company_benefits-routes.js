9bdb5326bee7fbebe9f14b4a5ae701c26ba28c75
felipezintzun <felipezintzun@gmail.com>
2022-02-19 12:20:05 -0600

    pushing up before changing all company_benefits to CoBenefits

--- controllers/api/company_benefits-routes.js
+++ controllers/api/company_benefits-routes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
-const { company_benefits, Benefits} = require('../../models');
+const { company_benefits, Benefits, Company} = require('../../models');
 
-// get all compnay_benefits
+// get all company_benefits
 router.get('/', (req, res) => {
   company_benefits.findAll(
-      {
-      attributes: ['id'],
-          include: [
-          {model: Company,
-          attributes: [ 'id', 'company_name' ]
-      },
-      {
-          modal: Benefits,
-          attributes: [ 'benefit_name']
-      }
-      ]
-  }
+      // {
+      // attributes: ['id'],
+      //     include: [
+      //     {model: Company,
+      //     attributes: [ 'id', 'company_name' ]
+      // },
+      // {
+      //     modal: Benefits,
+      //     attributes: [ 'id', 'benefit_name']
+      // }
+      // ]
+  // }
   )
   .then(dbcompany_benefitsData => res.json(dbcompany_benefitsData))
   .catch(err => {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,14 +5,14 @@ const companyRoutes = require('./company-routes');
 const userRoutes = require('./user-routes');
 const ratingsRoutes = require('./ratings-routes');
 const benefitsRoutes = require('./benefits-routes');
-const coBenfitsRoutes = require('./company_benefits-routes');
+const companyBenfitsRoutes = require('./company_benefits-routes');
 
 router.use('/reviews', reviewsRoutes);
 router.use('/companies', companyRoutes);
 router.use('/users', userRoutes);
 router.use('/ratings', ratingsRoutes);
 router.use('/benefits', benefitsRoutes);
-router.use('/company_benefits', coBenfitsRoutes);
+router.use('/company_benefits', companyBenfitsRoutes);
 
 
 module.exports = router;
