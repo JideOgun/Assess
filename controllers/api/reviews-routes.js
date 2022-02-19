99fe730ae54304710a9499911844e2eae925243b
michael Means <colemeans51@gmail.com>
2022-02-18 19:46:58 -0600

    starting PUT for company routes.js

--- controllers/api/reviews-routes.js
+++ controllers/api/reviews-routes.js
@@ -25,6 +25,32 @@ router.post('/', (req, res) => {
     }
 });
 
+router.put('/:id',  (req, res) => {
+    Reviews.update(
+        {
+            reviews_text: req.body.reviews_text,
+            user_id: req.session.user_id,
+            post_id: req.body.company_name
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      )
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No Review found with this id' });
+            return;
+          }
+          res.json(dbPostData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
+
 router.delete('/:id', (req, res) => {
     if (req.session) {
         Reviews.destroy({
