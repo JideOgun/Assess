a7add7f1b71db6d41bd0846e8c4019e8c8033e78
CraigJensen <cmjensen82@gmail.com>
2022-02-20 20:30:38 -0600

    added dashboard link to main page

--- seeds/companybenefits-seeds.js
+++ seeds/companybenefits-seeds.js
@@ -1,40 +1,41 @@
 const { CompanyBenefits } = require('../models');
 
 const CompanyBenefitsData = [
-    {
-       company_id: 3,
-       benefits_id: 2
-    },
-    {
-        company_id: 1,
-        benefits_id: 5
-    },
-    {
-        company_id: 4,
-        benefits_id: 2
-    },
-    {
-        company_id: 6,
-        benefits_id: 1
-    },
-    {
-        company_id: 2,
-        benefits_id: 3
-    },
-    {
-        company_id: 5,
-        benefits_id: 4
-    },
-    {
-        company_id: 1,
-        benefits_id: 7
-    },
-    {
-        company_id: 1,
-        benefits_id: 1
-    }
+  {
+    company_id: 3,
+    benefits_id: [2, 3, 5],
+  },
+  {
+    company_id: 1,
+    benefits_id: 5,
+  },
+  {
+    company_id: 4,
+    benefits_id: 2,
+  },
+  {
+    company_id: 6,
+    benefits_id: 1,
+  },
+  {
+    company_id: 2,
+    benefits_id: 3,
+  },
+  {
+    company_id: 5,
+    benefits_id: 4,
+  },
+  {
+    company_id: 1,
+    benefits_id: 7,
+  },
+  {
+    company_id: 1,
+    benefits_id: 1,
+  },
 ];
 
-const seedCompanyBenefits = () => CompanyBenefits.bulkCreate(CompanyBenefitsData);
+const seedCompanyBenefits = () =>
+  CompanyBenefits.bulkCreate(CompanyBenefitsData);
 
 module.exports = seedCompanyBenefits;
