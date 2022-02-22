d739b6813abdf138b93b5f39701b2e921faa55fd
CraigJensen <cmjensen82@gmail.com>
2022-02-22 16:26:40 -0600

    updating remote

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,49 +19,52 @@ router.get('/', (req, res) => {
       },
       {
         model: Benefits,
-        attributes: ['benefit_name']
-      }, 
+        attributes: ['benefit_name'],
+      },
       {
         model: Ratings,
-        attributes: ['rating_value']
-      }
-    ]
+        attributes: ['rating_value'],
+      },
+    ],
   })
     .then((dbCompanyData) => {
       const companies = dbCompanyData.map((company) =>
-        company.get({ plain: true }));
-// console.log(companies);
+        company.get({ plain: true })
+      );
+      // console.log(companies);
 
+      let reviewsArr = [];
+      for (var r = 0; r < companies.length; r++) {
+        const element = companies[r];
+        console.log(element.reviews.length);
+        reviewsArr.push(element.reviews);
+      }
+      let sum = 0;
+      let avgArr = [];
+      for (let i = 0; i < companies.length; i++) {
+        const element = companies[i];
+        let object = element.ratings;
+        //  console.log(element.ratings);
+        // console.log(object);
+        Object.keys(object).forEach((key) => {
+          // console.log(key);
+          if (key == 0) {
+            sum = 0;
+          }
+          sum += object[key].rating_value;
+        });
+        console.log(sum);
+        let avg = (sum / companies[i].ratings.length).toFixed(1);
+        avgArr.push(avg);
+      }
+      console.log(avgArr);
 
-let reviewsArr = [];
-for(var r = 0; r < companies.length; r++) {
-  const element = companies[r];
-  console.log(element.reviews.length);
-  reviewsArr.push(element.reviews);
-}
-        let sum = 0;
-        let avgArr = [];
-        for (let i = 0; i < companies.length; i++) {
-                    const element = companies[i];
-                    let object = element.ratings; 
-                  //  console.log(element.ratings);
-// console.log(object);
-                  Object.keys(object).forEach(key => {
-                    // console.log(key);
-                    if(key == 0) {
-                      sum = 0; 
-                    }
-                    sum += object[key].rating_value;
-                   
-                  }); console.log(sum);
-                  let avg = (sum/companies[i].ratings.length).toFixed(1);
-                  avgArr.push(avg);
-                  
-                }
-                console.log(avgArr);
-                 
-
-      res.render('homepage', { companies, avgArr, reviewsArr, loggedIn: req.session.loggedIn });
+      res.render('homepage', {
+        companies,
+        avgArr,
+        reviewsArr,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -76,8 +79,6 @@ router.get('/login', (req, res) => {
   }
   res.render('login');
 });
-
-
 
 router.get('/company/:id', (req, res) => {
   Company.findOne({
@@ -100,12 +101,12 @@ router.get('/company/:id', (req, res) => {
       },
       {
         model: Benefits,
-        attributes: ['benefit_name']
-      }, 
+        attributes: ['benefit_name'],
+      },
       {
         model: Ratings,
-        attributes: ['rating_value']
-      }
+        attributes: ['rating_value'],
+      },
     ],
   })
     .then((dbCompanyData) => {
@@ -113,26 +114,28 @@ router.get('/company/:id', (req, res) => {
         res.status(404).json({ message: 'No company with that ID' });
         return;
       }
-      
+
       const companies = dbCompanyData.get({ plain: true });
       console.log(companies);
-let sum = 0;
+      let sum = 0;
       for (let i = 0; i < companies.ratings.length; i++) {
-                  const element = companies.ratings[i]; 
-                 sum += element.rating_value;
-              }
-              // console.log(sum);
-               let avg = (sum/companies.ratings.length).toFixed(1);
-             
-              // console.log(avg);  
-      res.render('single-company', { companies, avg, loggedIn: req.session.loggedIn  });
+        const element = companies.ratings[i];
+        sum += element.rating_value;
+      }
+      // console.log(sum);
+      let avg = (sum / companies.ratings.length).toFixed(1);
+
+      // console.log(avg);
+      res.render('single-company', {
+        companies,
+        avg,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-
-
 
 module.exports = router;
