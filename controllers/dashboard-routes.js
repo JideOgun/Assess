1df19379211d88813ab77da4a0356be50202750b
JideOgun <JideOgun@github.com>
2022-02-21 23:15:27 -0600

    added withAuth

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,36 +3,18 @@ const sequelize = require('../config/connection');
 const { Company, Reviews } = require('../models');
 
 router.get('/', (req, res) => {
-      console.log("dashboard", req.session)
+      console.log("dashboard", req.session);
       if (req.session.loggedIn) {
-        // Company.findAll()
-        //   .then(allCompany => {
-              Reviews.findAll({
-                  where: {
-                  user_id: req.session.user_id
-                  }
-                })
-                .then(reviewdata => {
-                  const reviewAll = reviewdata.map(review =>
-                    review.get({
-                      plain: true
-                    })
-                    )
-                  // const companyData = allCompany.map(company =>
-                  //   company.get({
-                  //     plain: true
-                  //   })
-                  // );
-                  res.render('dashboard', {
-                    loggedIn: true,
-                    // data: companyData,
-                    review: reviewAll
-                  });
-                })
-              // });
-            } else {
-              res.redirect('/login');
-            }
-          });
+      Reviews.findAll({
+         where: {user_id: req.session.user_id}
+          })
+    .then(reviewdata => {
+        const reviewAll = reviewdata.map(review =>
+          review.get({ plain: true })
+         );
+       res.render('dashboard', {loggedIn: true, review: reviewAll});
+        });
+  } else { res.redirect('/login');}
+   });
 
       module.exports = router;
