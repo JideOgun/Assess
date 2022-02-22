36067cc9013efb7958a84c6da2bf45460c94c1b7
Judy <judy.mtz1@gmail.com>
2022-02-22 13:22:01 -0600

    fixed bug with seeds

--- seeds/company-seeds.js
+++ seeds/company-seeds.js
@@ -39,67 +39,67 @@ const companyData = [
   {
     company_name: 'Samsung',
     roles: "Data Center Engineer",
-    user_id: 8
+    user_id: 7
   },
   {
     company_name: 'YouTube',
     roles: "Software Engineer",
-    user_id: 9
+    user_id: 6
   },
   {
     company_name: 'Microsoft',
     roles: "Software Engineer",
-    user_id: 10
+    user_id: 7
   },
   {
     company_name: 'Dell',
     roles: "Data Scientist",
-    user_id: 11
+    user_id: 5
   },
   {
     company_name: 'IBM',
     roles: "Test Automation Developer",
-    user_id: 12
+    user_id: 4
   },
   {
     company_name: 'Cisco',
     roles: "Senior Backend Engineer",
-    user_id: 13
+    user_id: 3
   },
   {
     company_name: 'Sony',
     roles: "IT/DevOps Engineer",
-    user_id: 14
+    user_id: 2
   },
   {
     company_name: 'DataVox',
     roles: "Network Engineer",
-    user_id: 15
+    user_id: 1
   },
   {
     company_name: 'Adobe',
     roles: "Software Development Engineer",
-    user_id: 16
+    user_id: 5
   },
   {
     company_name: 'Amazon',
     roles: "Systems Development Engineer",
-    user_id: 17
+    user_id: 7
   },
   {
     company_name: 'Zynga',
     roles: "Senior Cyber Security Architect",
-    user_id: 18
+    user_id: 6
   },
   {
     company_name: 'Atlassian',
     roles: "Frontend Software Engineer",
-    user_id: 19
+    user_id: 5
   },
   {
     company_name: 'HP',
     roles: "IT Developer & Support Engineer",
-    user_id: 20
+    user_id: 4
   }
 
 ];
