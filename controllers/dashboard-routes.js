d70f0fa03a18681954cf1e11988b6660f4e44707
felipezintzun <felipezintzun@gmail.com>
2022-02-21 21:24:55 -0600

    adjusted dashbord handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,38 +1,47 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Company, Reviews } = require('../models');
+const { Company, Reviews, Users } = require('../models');
 
 router.get('/', (req, res) => {
-      console.log("dashboard", req.session)
-      if (req.session.loggedIn) {
-        // Company.findAll()
-        //   .then(allCompany => {
-              Reviews.findAll({
-                  where: {
-                  user_id: req.session.user_id
-                  }
-                })
-                .then(reviewdata => {
-                  const reviewAll = reviewdata.map(review =>
-                    review.get({
-                      plain: true
-                    })
-                    )
-                  // const companyData = allCompany.map(company =>
-                  //   company.get({
-                  //     plain: true
-                  //   })
-                  // );
-                  res.render('dashboard', {
-                    loggedIn: true,
-                    // data: companyData,
-                    review: reviewAll
-                  });
-                })
-              // });
-            } else {
-              res.redirect('/login');
-            }
-          });
+  console.log("dashboard", req.session)
+  if (req.session.loggedIn) {
+    // Company.findAll()
+    //   .then(allCompany => {
+          Reviews.findAll({
+              where: {
+              user_id: req.session.user_id
+              },
+              attributes: [
+                'id',
+                'reviews_text',
+                'created_at'
+
+              ],
+              include: {
+                model: Reviews,
+                attributes: ['id', 'reviews_text', 'created_at'],
+              },
+              include: [
+                {
+                model: Company,
+                attributes: [ 'company_name', 'id']
+                }
+              ]
+            })
+            .then(reviewData => {
+              const reviewAll = reviewData.map(review => review.get({ plain: true }))
+              // const companyData = allCompany.map(company =>
+              //   company.get({
+              //     plain: true
+              //   })
+              // );
+              res.render('dashboard', { loggedIn: true, review: reviewAll });
+                // data: companyData,
+              })
+          // });
+        } else {
+          res.redirect('/login');
+        }
+      });
 
       module.exports = router;
