b26ee000a82715c9b00056d1cf2a73f9a62a21da
Judy <judy.mtz1@gmail.com>
2022-02-22 19:52:26 -0600

    added 6 more user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -60,7 +60,37 @@ const userData = [
         username: 'Kylian Mbappe',
         email: 'KylianM@gmail.com',
         password: 'KylianMbappe'
-    }
+    },
+    {
+        username: 'Ronaldinho',
+        email: 'Ronaldinho@gmail.com',
+        password: 'Ronaldinho'
+    },
+    {
+        username: 'Neymar',
+        email: 'Neymar@gmail.com',
+        password: 'Neymar'
+    },
+    {
+        username: 'Bob Ross',
+        email: 'BobR@gmail.com',
+        password: 'BobRoss'
+    },
+    {
+        username: 'Betty White',
+        email: 'BettyW@gmail.com',
+        password: 'BettyWhite'
+    },
+    {
+        username: 'Vanessa Bryant',
+        email: 'VanessaB@gmail.com',
+        password: 'VanessaBryant'
+    },
+    {
+        username: 'Bob Deckard',
+        email: 'BobD@gmail.com',
+        password: 'BobDeckard'
+    },
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
