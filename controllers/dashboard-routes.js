41dd83f67d5cb3ea3df423e30e9d42626a0ddaad
JideOgun <JideOgun@github.com>
2022-02-23 10:47:52 -0600

    rendering edit-review page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Company, Reviews, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
       console.log("dashboard", req.session);
@@ -29,5 +30,42 @@ router.get('/', (req, res) => {
         });
   } else { res.redirect('/login');}
    });
+
+
+   router.get('/edit/:id', withAuth, (req, res) => {
+      Reviews.findByPk(req.params.id, {
+          include: [
+              {
+                model: Company,
+                attributes: ['id', 'user_id', 'createdAt'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                }
+              },
+              {
+                model: User,
+                attributes: ['username']
+              }
+            ]
+      })
+      .then(dbReviewsData => {
+          if (dbReviewsData) {
+              const reviews = dbReviewsData.get({ plain: true });
+              console.log(reviews)
+              res.render('edit-review', {
+                reviews,
+                loggedIn: true
+              });
+            } else {
+              res.status(404).end();
+            }
+          })
+          .catch(err => {
+            res.status(500).json(err);
+          });  
+      });
+  
+
 
       module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes.js');
-// const ratingsRoutes = require('./ratings-routes');
+
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-// router.use('/company/', ratingsRoutes);
+
 
 module.exports = router;
