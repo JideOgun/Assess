e15777fc413ac2f289127420c971a25f6d6a6ea4
Judy <judy.mtz1@gmail.com>
2022-02-22 21:25:18 -0600

    added rating seeds

--- seeds/ratings-seeds.js
+++ seeds/ratings-seeds.js
@@ -46,6 +46,61 @@ const RatingsData = [
     user_id: 9,
     company_id: 9,
     rating_value: 5,
+  },
+  {
+    user_id: 10,
+    company_id: 10,
+    rating_value: 5,
+  },
+  {
+    user_id: 11,
+    company_id: 11,
+    rating_value: 5,
+  },
+  {
+    user_id: 12,
+    company_id: 12,
+    rating_value: 1,
+  },
+  {
+    user_id: 13,
+    company_id: 13,
+    rating_value: 5,
+  },
+  {
+    user_id: 14,
+    company_id: 14,
+    rating_value: 4,
+  },
+  {
+    user_id: 15,
+    company_id: 15,
+    rating_value: 5,
+  },
+  {
+    user_id: 16,
+    company_id: 16,
+    rating_value: 3,
+  },
+  {
+    user_id: 17,
+    company_id: 17,
+    rating_value: 1,
+  },
+  {
+    user_id: 18,
+    company_id: 18,
+    rating_value: 4,
+  },
+  {
+    user_id: 19,
+    company_id: 19,
+    rating_value: 5,
+  },
+  {
+    user_id: 20,
+    company_id: 20,
+    rating_value: 3,
   }
   
 ];
