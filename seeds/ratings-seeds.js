acfc2c866210286c1830249a31c7ab1e57a4fcda
Judy <judy.mtz1@gmail.com>
2022-02-22 21:12:27 -0600

    added rating seeds

--- seeds/ratings-seeds.js
+++ seeds/ratings-seeds.js
@@ -6,14 +6,28 @@ const RatingsData = [
     user_id: 1,
     company_id: 2,
     rating_value: 5,
-    
   },
   {
     user_id: 2,
     company_id: 1,
     rating_value: 4,
-    
+  },
+  {
+    user_id: 3,
+    company_id: 3,
+    rating_value: 5,
+  },
+  {
+    user_id: 4,
+    company_id: 4,
+    rating_value: 1,
+  },
+  {
+    user_id: 5,
+    company_id: 5,
+    rating_value: 5,
   }
+  
 ];
 
 const seedRatings = () => Ratings.bulkCreate(RatingsData);
