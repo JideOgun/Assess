44f7b1c015be53312219dc4f0f4b08b3ebf0d69b
Judy <judy.mtz1@gmail.com>
2022-02-24 15:55:40 -0600

    added expedia benefit seeds

--- seeds/companybenefits-seeds.js
+++ seeds/companybenefits-seeds.js
@@ -99,19 +99,39 @@ const CompanyBenefitsData = [
   },
   {
     company_id: 3,
+    benefits_id: 3,
+  },
+  {
+    company_id: 3,
+    benefits_id: 4,
+  },
+  {
+    company_id: 3,
+    benefits_id: 5,
+  },
+  {
+    company_id: 3,
+    benefits_id: 6,
+  },
+  {
+    company_id: 3,
+    benefits_id: 7,
+  },
+  {
+    company_id: 3,
+    benefits_id: 8,
+  },
+  {
+    company_id: 3,
+    benefits_id: 9,
+  },
+  {
+    company_id: 3,
+    benefits_id: 10,
+  },
+  {
+    company_id: 3,
     benefits_id: 11,
-  },
-  {
-    company_id: 3,
-    benefits_id: 13,
-  },
-  {
-    company_id: 3,
-    benefits_id: 15,
-  },
-  {
-    company_id: 3,
-    benefits_id: 17,
   },
 ];
 
