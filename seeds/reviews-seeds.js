4dfdd6e15cfe3b350f796ae7a35a175ae3b21c2f
Judy <judy.mtz1@gmail.com>
2022-02-24 01:31:57 -0600

    added seeds to datavox

--- seeds/reviews-seeds.js
+++ seeds/reviews-seeds.js
@@ -881,6 +881,56 @@ const reviewsData = [
         user_id: 7,
         company_id: 14
     },
+    {
+        reviews_text: `Great benefits`,
+        user_id: 8,
+        company_id: 15 
+    },
+    {
+        reviews_text: `Management is horrific`,
+        user_id: 9,
+        company_id: 15 
+    },
+    {
+        reviews_text: `Great office space`,
+        user_id: 10,
+        company_id: 15
+    },
+    {
+        reviews_text: `Layed back office envioronment`,
+        user_id: 11,
+        company_id: 15
+    },
+    {
+        reviews_text: `STAY AWAY, DO NOT WORK HERE`,
+        user_id: 12,
+        company_id: 15
+    },
+    {
+        reviews_text: `If you can't stand micromanaging, don't work here`,
+        user_id: 13,
+        company_id: 15
+    },
+    {
+        reviews_text: `Work from home options`,
+        user_id: 14,
+        company_id: 15
+    },
+    {
+        reviews_text: `Lots of corporate discounts yay!`,
+        user_id: 15,
+        company_id: 15
+    },
+    {
+        reviews_text: `Flexible schedules`,
+        user_id: 16,
+        company_id: 15
+    },
+    {
+        reviews_text: `Very strict working hours`,
+        user_id: 17,
+        company_id: 15
+    },
 
 
 ];
