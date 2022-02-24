34341c4450e2603743c9192225026a353e804295
Judy <judy.mtz1@gmail.com>
2022-02-24 16:43:39 -0600

    added company benefit seeds

--- seeds/companybenefits-seeds.js
+++ seeds/companybenefits-seeds.js
@@ -381,6 +381,38 @@ const CompanyBenefitsData = [
     company_id: 10,
     benefits_id: 10,
   },
+  {
+    company_id: 11,
+    benefits_id: 11,
+  },
+  {
+    company_id: 11,
+    benefits_id: 12,
+  },
+  {
+    company_id: 11,
+    benefits_id: 13,
+  },
+  {
+    company_id: 11,
+    benefits_id: 14,
+  },
+  {
+    company_id: 11,
+    benefits_id: 15,
+  },
+  {
+    company_id: 11,
+    benefits_id: 16,
+  },
+  {
+    company_id: 11,
+    benefits_id: 17,
+  },
+  {
+    company_id: 11,
+    benefits_id: 18,
+  },
 
 ];
 
