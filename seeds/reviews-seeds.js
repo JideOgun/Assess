167035c0db84162264b1085af5c41c42df6dbae1
Judy <judy.mtz1@gmail.com>
2022-02-24 01:18:59 -0600

    added seeds to microsoft

--- seeds/reviews-seeds.js
+++ seeds/reviews-seeds.js
@@ -676,6 +676,41 @@ const reviewsData = [
         user_id: 12,
         company_id: 9
     },
+    {
+        reviews_text: 'Great office space',
+        user_id: 13,
+        company_id: 10
+    },
+    {
+        reviews_text: 'Layed back office envioronment',
+        user_id: 14,
+        company_id: 10
+    },
+    {
+        reviews_text: 'STAY AWAY, DO NOT WORK HERE',
+        user_id: 15,
+        company_id: 10
+    },
+    {
+        reviews_text: `If you can't stand micromanaging, don't work here`,
+        user_id: 16,
+        company_id: 10
+    },
+    {
+        reviews_text: `Work from home options`,
+        user_id: 17,
+        company_id: 10
+    },
+    {
+        reviews_text: `Lots of corporate discounts yay!`,
+        user_id: 18,
+        company_id: 10
+    },
+    {
+        reviews_text: `Flexible schedules`,
+        user_id: 19,
+        company_id: 10
+    },
 
 
 ];
